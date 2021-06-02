3b23304a5d2566acf5c0bd48f5682627ff5a8472
rainickmr <rainickmr@gmail.com>
2021-06-02 17:54:36 -0300

    removendo classes e deixando direto function

--- app/controller/PlanetaController.js
+++ app/controller/PlanetaController.js
@@ -6,66 +6,57 @@ const ObjectId = require('mongodb').ObjectID;
 const idError = 'Invalid id. Id must be a single String of 12 bytes or a string of 24 hex characters'
 const swapiUri = 'https://swapi.dev/api/planets/'
 
-class PlanetaService {
-    constructor() {
-    }
 
-    _sendErrorsResponse(res, errors) {
-        return res
-            .status(400)
-            .json({ errors: [...errors] })
-    }
+sendErrorsResponse = (res, errors) => {
+    return res
+        .status(400)
+        .json({ errors: [...errors] })
+}
 
-    createFilter(requestQuery) {
-        const filter = {}
-        if (requestQuery.nome) {
-            filter.nome = requestQuery.nome
-        }
-        if (requestQuery.clima) {
-            filter.clima = requestQuery.clima
-        }
-        if (requestQuery.terreno) {
-            filter.terreno = requestQuery.terreno
-        }
-        return filter
+createFilter = (requestQuery) => {
+    const filter = {}
+    if (requestQuery.nome) {
+        filter.nome = requestQuery.nome
+    }
+    if (requestQuery.clima) {
+        filter.clima = requestQuery.clima
+    }
+    if (requestQuery.terreno) {
+        filter.terreno = requestQuery.terreno
+    }
+    return filter
+}
+
+createResponseJson = (data, quantidadeAparicoes) => {
+    return {
+        _id: data._id,
+        nome: data.nome,
+        clima: data.clima,
+        terreno: data.terreno,
+        quantidadeAparicoes: quantidadeAparicoes
     }
 }
 
-class SwaiapiService {
-    async getQuantidadeAparicoesPlaneta(name) {
-        const url = `${swapiUri}?search=${name}`
-        const apiResponse = await axios.get(url)
-        let quantidadeAparicoes = 0
-        if (apiResponse.data.results.length == 1) {
-            const planetaSearched = apiResponse.data.results[0]
-            quantidadeAparicoes = planetaSearched.films.length ? planetaSearched.films.length : 0
-        }
-        return quantidadeAparicoes
+getQuantidadeAparicoesPlaneta = async (name) => {
+    const url = `${swapiUri}?search=${name}`
+    const apiResponse = await axios.get(url)
+    let quantidadeAparicoes = 0
+    if (apiResponse.data.results.length == 1) {
+        const planetaSearched = apiResponse.data.results[0]
+        quantidadeAparicoes = planetaSearched.films.length ? planetaSearched.films.length : 0
     }
-
-    createResponseJson(data, quantidadeAparicoes) {
-        return {
-            _id: data._id,
-            nome: data.nome,
-            clima: data.clima,
-            terreno: data.terreno,
-            quantidadeAparicoes: quantidadeAparicoes
-        }
-    }
+    return quantidadeAparicoes
 }
-
-const planetaService = new PlanetaService()
-const swaiapiService = new SwaiapiService()
 
 class PlanetaController {
     async list(req, res) {
-        const filter = planetaService.createFilter(req.query)
+        const filter = createFilter(req.query)
 
         const data = await Planeta.find(filter)
 
         const resultArray = await Promise.all(data.map(async (i) => {
-            const quantidadeAparicoesPlaneta = await swaiapiService.getQuantidadeAparicoesPlaneta(i.nome)
-            return swaiapiService.createResponseJson(i, quantidadeAparicoesPlaneta)
+            const quantidadeAparicoesPlaneta = await getQuantidadeAparicoesPlaneta(i.nome)
+            return createResponseJson(i, quantidadeAparicoesPlaneta)
         }));
 
         return res.json(resultArray)
@@ -75,15 +66,15 @@ class PlanetaController {
         const id = req.params.id
 
         if (!ObjectId.isValid(id)) {
-            return planetaService._sendErrorsResponse(res, [idError])
+            return sendErrorsResponse(res, [idError])
         }
 
         const query = { "_id": ObjectId(id) }
         const data = await Planeta.findOne(query)
 
-        const quantidadeAparicoesPlaneta = await swaiapiService.getQuantidadeAparicoesPlaneta(data.nome)
+        const quantidadeAparicoesPlaneta = await getQuantidadeAparicoesPlaneta(data.nome)
 
-        return res.json(swaiapiService.createResponseJson(data, quantidadeAparicoesPlaneta))
+        return res.json(createResponseJson(data, quantidadeAparicoesPlaneta))
     }
 
     async create(req, res) {
@@ -96,7 +87,7 @@ class PlanetaController {
         const id = req.params.id
 
         if (!ObjectId.isValid(id)) {
-            return planetaService._sendErrorsResponse(res, [idError])
+            return sendErrorsResponse(res, [idError])
         }
 
         const query = { "_id": ObjectId(id) }
@@ -112,7 +103,7 @@ class PlanetaController {
         const id = req.params.id
 
         if (!ObjectId.isValid(id)) {
-            return planetaService._sendErrorsResponse(res, [idError])
+            return sendErrorsResponse(res, [idError])
         }
 
         const query = { "_id": ObjectId(id) }
